17181028b92da07321949bcf7f811081db81c167
110b13061 <113488308+110b13061@users.noreply.github.com>
2022-10-12 20:36:58 +0800

    Create  Class06.java

---
+++ Class06.java
@@ -0,0 +1,27 @@
+package java10;
+class CTriangle{
+	protected int base;
+	protected int height;
+	protected void show()
+	{
+		System.out.println("base="+base+", height="+height);
+	}
+}
+class CData extends CTriangle{           //(a)
+	public void area() {                //(b)
+		show();
+		System.out.println("area="+base*height/2);
+	}
+	public CData(int a,int b)
+	{
+		base=a;
+		height=b;
+	}
+}
+public class Class06 {
+	public static void main(String[] args)	//(c)
+	{
+		CData obj=new CData(3,8);
+		obj.area();
+	}
+}
